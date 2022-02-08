9cf82e85d24addccbd630ccf9ece03f1c82ee623
ChangQi-555 <64196386+ChangQi-555@users.noreply.github.com>
2022-02-09 01:16:41 +0800

    Update application.c

--- TinyTimber/RTS-Lab/application.c
+++ TinyTimber/RTS-Lab/application.c
@@ -1,3 +1,35 @@
+
+/*
+User guide:
+Compile and upload the .s19 file to the experiment board, type "go 20000000" to start the execution.
+Use keyboard to input or clear numbers for three-history function. Type positive or negative numbers on keyboard and type an 'e' to represent the end of input. 
+After inputing each number, the application will print out the current input, the sum and the median of the numbers.
+'F' is used to clear the history of numbers. 
+
+Example input:
+F 13e -7e 20e -1e
+
+Example output:
+Rcv: ’F’
+The 3-history has been erased
+Rcv: ’1’
+Rcv: ’3’
+Rcv: ’e’
+Entered integer 13: sum = 13, median = 13
+Rcv: ’-’
+Rcv: ’7’
+Rcv: ’e’
+Entered integer -7: sum = 6, median = 3
+Rcv: ’2’
+Rcv: ’0’
+Rcv: ’e’
+Entered integer 20: sum = 26, median = 13
+Rcv: ’-’
+Rcv: ’1’
+Rcv: ’e’
+Entered integer -1: sum = 12, median = -1
+*/
+
 #include "TinyTimber.h"
 #include "sciTinyTimber.h"
 #include "canTinyTimber.h"
